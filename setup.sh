f5495461887ebd2d4607a8de932ac4c3ff2290b5
siva <siva@email.com>
2024-06-30 19:35:12 -0400

    ADD: setup.sh without needing to be in root dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-[ -e ./env ] || python -m venv env 
-source ./env/bin/activate
-pip install -r ./requirements.txt
+ROOT=$1
+[ -z "$ROOT" ] && ROOT="." && echo "making $(pwd) root"
+[ -e $ROOT/env ] || python -m venv env 
+source $ROOT/env/bin/activate
+pip install -r $ROOT/requirements.txt
 
